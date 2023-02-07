0f0a0c656c4f7cdb00ed8987f7d56e19e68166a2
Eya-Tom Augustin SANGAM <augustin.sangam@gmail.com>
2023-02-07 15:07:13 -0500

    Update metrics benchmarks

--- results/compile_metrics_benchmarks.py
+++ results/compile_metrics_benchmarks.py
@@ -1,7 +1,7 @@
 import re
 import statistics
 import sys
-from collections import OrderedDict
+from collections import OrderedDict, defaultdict
 from pathlib import Path
 
 if __name__ == "__main__":
@@ -15,13 +15,31 @@ if __name__ == "__main__":
                 continue
             if not executable:
                 continue
-            match = re.match(r"duration\s*=\s*(?P<duration>\d+)\s*ns", line)
-            if not match:
-                continue
-            if executable not in executables_and_durations:
-                executables_and_durations[executable] = []
-            executables_and_durations[executable].append(int(match.group("duration")))
+            math_stats_and_regex = [
+                ("duration", r"duration\s*=\s*(?P<duration>\d+)\s*ns"),
+            ]
+            for stat, regex in math_stats_and_regex:
+                match = re.match(regex, line)
+                if not match:
+                    continue
+                if executable not in executables_and_durations:
+                    executables_and_durations[executable] = defaultdict(list)
+                executables_and_durations[executable][stat].append(float(match.group("duration")))
+            time_stats_and_regex = [
+                ("real", r"real\s*(?P<m>[+-]?([0-9]*[.])?[0-9]+)m(?P<s>[+-]?([0-9]*[.])?[0-9]+)s"),
+                ("user", r"user\s*(?P<m>[+-]?([0-9]*[.])?[0-9]+)m(?P<s>[+-]?([0-9]*[.])?[0-9]+)s"),
+                ("sys", r"sys\s*(?P<m>[+-]?([0-9]*[.])?[0-9]+)m(?P<s>[+-]?([0-9]*[.])?[0-9]+)s"),
+            ]
+            for stat, regex in time_stats_and_regex:
+                match = re.match(regex, line)
+                if not match:
+                    continue
+                if executable not in executables_and_durations:
+                    executables_and_durations[executable] = defaultdict(list)
+                duration = (float(match.group("m")) * 60 + float(match.group("s"))) * 1000000000
+                executables_and_durations[executable][stat].append(duration)
     with Path(sys.argv[2]).open("w") as f_out:
-        for executable, durations in executables_and_durations.items():
+        for executable, stats in executables_and_durations.items():
             f_out.write(f"{executable}\n")
-            f_out.write(f"duration = {statistics.mean(durations)}\n")
+            for stat, val in stats.items():
+                f_out.write(f"{stat} = {statistics.mean(val)}\n")
